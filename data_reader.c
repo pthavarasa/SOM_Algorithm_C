a55a347af882b7fed41c3eeee7c33edd92e452f2
Thavarasa Prasanth <selvamkana@yahoo.com>
2021-11-23 00:27:17 +0100

    read data from iris.data

--- data_reader.c
+++ data_reader.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main(){
-    
-    return 0;
+int main(){
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    size_t read;
+
+    fp = fopen("iris.data", "r");
+    if (fp == NULL){
+        printf("File not found!\n");
+        return 0;
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    if (line) free(line);
+    return 1;
 }
